de70dfa2a21ddc8aa5e683386abdde00f80bb9a8
zxyandreay <zxyandreay@gmail.com>
2024-04-12 07:58:00 +0800

    Updated

--- push.bat
+++ push.bat
@@ -1,5 +1,14 @@
 @echo off
+echo Changing directory to the Git repository...
 cd /d D:\!Main\Class\!Thesis\prototype
+
+echo Adding all changes to staging...
 git add .
+
+echo Committing the changes...
 git commit -m "Updated"
+
+echo Pushing changes to the remote repository...
 git push origin main
+
+echo Update complete.
