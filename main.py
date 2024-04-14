42449cd57f7d3a9d1e1fb8978b58629b957f62af
zxyandreay <zxyandreay@gmail.com>
2024-04-14 20:17:22 +0800

    Updated

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ model = joblib.load('model/sockpuppet_model.pkl')
 tfidf = joblib.load('model/tfidf_vectorizer.pkl')
 
 def preprocess(text):
-    text = text.lower()
+    text = text.lower()  # Convert text to lowercase
     text = re.sub(r'\[.*?\]|\(.*?\)|\{.*?\}|\<.*?\>|https?://\S+|www\.\S+|<.*?>', '', text)
-    text = re.sub(r'\W|\d+', '', text)
+    text = re.sub(r'\W|\d+', '', text)  # Remove non-alphanumeric characters and numbers
     return text
 
 @app.route('/')
@@ -32,13 +32,16 @@ def predict():
         polarity = TextBlob(processed_text).sentiment.polarity
         subjectivity = TextBlob(processed_text).sentiment.subjectivity
         X_input = np.hstack((X_input_tfidf, np.array([[polarity, subjectivity]])))
+
         prediction = model.predict(X_input)[0]
         result = 'sockpuppet' if prediction == 1 else 'non-sockpuppet'
+
     except Exception as e:
         app.logger.error(f"Error during prediction: {e}", exc_info=True)
         return jsonify({'error': str(e)}), 500
+
     return render_template('result.html', prediction=result)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)  # Use debug=False for production
